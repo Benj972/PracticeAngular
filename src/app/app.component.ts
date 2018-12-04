0991a6ed6c8d69ca621a7b230aadcd32df192d09
Benj972 <ben.gallot972@gmail.com>
2018-12-04 13:54:50 +0100

    create variable posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  posts = [
+  {
+
+  	title: 'Post1',
+  	content: 'Dein digerentibus conpage agmina dein subsidiales totum digerentibus ita vicinitate castrensi castrensi agmina ostendit aptatae omnium sollicite distortaque textrinum naturae.',
+  	loveIts: 2,
+  	created_at: new Date('December 04, 2018 11:00:00')
+  },
+  {
+  	title: 'Post2',
+  	content: 'Dein digerentibus conpage agmina dein subsidiales totum digerentibus ita vicinitate castrensi castrensi agmina ostendit aptatae omnium sollicite distortaque textrinum naturae.',
+  	loveIts: -1,
+  	created_at: new Date('December 04, 2018 11:10:00')
+  },
+  {
+  	title: 'Post3',
+  	content: 'Dein digerentibus conpage agmina dein subsidiales totum digerentibus ita vicinitate castrensi castrensi agmina ostendit aptatae omnium sollicite distortaque textrinum naturae.',
+  	loveIts: 0,
+  	created_at: new Date('December 04, 2018 11:17:03')
+  }
+  ];
 }
